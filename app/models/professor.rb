a37452ba948551517af822fd431faaab170fba44
Fred Rondina <fredrondina96@gmail.com>
2020-03-27 10:30:08 -0600

    Completed User Story 4 + Tests

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -2,4 +2,12 @@ class Professor <ApplicationRecord
   validates_presence_of :name, :age, :specialty
   has_many :professor_students
   has_many :students, through: :professor_students
+
+  def average_student_age
+    average_age = 0.0
+      self.students.each do |student|
+        average_age += student.age
+      end
+    return (average_age/self.students.length).round(1)
+  end
 end
--- spec/features/professors/show_page_displays_professors_students_spec.rb
+++ spec/features/professors/show_page_displays_professors_students_spec.rb
@@ -15,4 +15,17 @@ RSpec.describe "When I visit a Professors show page", type: :feature do
     expect(page).to have_content(harry.name)
     expect(page).to have_content(malfoy.name)
   end
+
+  it "displays the average age of the professors students" do
+    hagarid = Professor.create(name: "Rubus Hagarid", age: 38 , specialty: "Care of Magical Creatures")
+    harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+    malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+
+    ProfessorStudent.create(student: harry, professor: hagarid)
+    ProfessorStudent.create(student: malfoy, professor: hagarid)
+
+    visit "/professors/#{hagarid.id}"
+
+    expect(page).to have_content("Average Student Age: 11.5")
+  end
 end
